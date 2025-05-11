9989d6bb8d20677e2dd3afccab8a9fdb22b26024
yuan_qi <152702577+yuan-qi5@users.noreply.github.com>
2025-05-11 15:21:38 +0800

    Update model_llama4drive.py

--- llama2/model_llama4drive.py
+++ llama2/model_llama4drive.py
@@ -84,7 +84,7 @@ def _make_causal_mask(
     bsz, tgt_len = input_ids_shape
     mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
     mask_cond = torch.arange(mask.size(-1), device=device)
-    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0) # 巧妙的广播
     mask = mask.to(dtype)
 
     if past_key_values_length > 0:
